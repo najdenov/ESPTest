cb46b127ea4bd05b388062d308b309f4b49f17cc
najdenov <najdenov@gmail.com>
2016-01-04 20:29:45 +0200

    ESP8266 Test App

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,88 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+        "id": "cordova-plugin-console.console",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-console/www/logger.js",
+        "id": "cordova-plugin-console.logger",
+        "pluginId": "cordova-plugin-console",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-chrome-apps-common/events.js",
+        "id": "cordova-plugin-chrome-apps-common.events",
+        "pluginId": "cordova-plugin-chrome-apps-common",
+        "clobbers": [
+            "chrome.Event"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-chrome-apps-common/errors.js",
+        "id": "cordova-plugin-chrome-apps-common.errors",
+        "pluginId": "cordova-plugin-chrome-apps-common"
+    },
+    {
+        "file": "plugins/cordova-plugin-chrome-apps-common/stubs.js",
+        "id": "cordova-plugin-chrome-apps-common.stubs",
+        "pluginId": "cordova-plugin-chrome-apps-common"
+    },
+    {
+        "file": "plugins/cordova-plugin-chrome-apps-common/helpers.js",
+        "id": "cordova-plugin-chrome-apps-common.helpers",
+        "pluginId": "cordova-plugin-chrome-apps-common"
+    },
+    {
+        "file": "plugins/cordova-plugin-chrome-apps-sockets-tcp/sockets.tcp.js",
+        "id": "cordova-plugin-chrome-apps-sockets-tcp.sockets.tcp",
+        "pluginId": "cordova-plugin-chrome-apps-sockets-tcp",
+        "clobbers": [
+            "chrome.sockets.tcp"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-chrome-apps-sockets-tcpserver/sockets.tcpServer.js",
+        "id": "cordova-plugin-chrome-apps-sockets-tcpserver.sockets.tcpServer",
+        "pluginId": "cordova-plugin-chrome-apps-sockets-tcpserver",
+        "clobbers": [
+            "chrome.sockets.tcpServer"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-background-app/backgroundapp.js",
+        "id": "cordova-plugin-background-app.backgroundapp",
+        "pluginId": "cordova-plugin-background-app",
+        "clobbers": [
+            "cordova.backgroundapp"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "cordova-plugin-device": "1.1.0",
+    "cordova-plugin-console": "1.0.2",
+    "cordova-plugin-chrome-apps-common": "1.0.7",
+    "cordova-plugin-chrome-apps-iossocketscommon": "1.0.2",
+    "cordova-plugin-chrome-apps-sockets-tcp": "1.3.4",
+    "cordova-plugin-chrome-apps-sockets-tcpserver": "1.2.3",
+    "cordova-plugin-background-app": "2.0.2"
+}
+// BOTTOM OF METADATA
+});
